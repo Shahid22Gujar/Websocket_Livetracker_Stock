d0134c0a1611b303282036220041fa410eb14dc9
Shahid22Gujar <shahidahimadgujar86@gmail.com>
2022-11-17 20:05:28 +0530

    asgi update which danphe cli error

--- stockproject/asgi.py
+++ stockproject/asgi.py
@@ -6,8 +6,11 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
+#problme while deploying => https://stackoverflow.com/questions/71119024/improperlyconfigured-requested-setting-installed-apps-but-settings-are-not-con
 
 import os
+import django
+
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter,URLRouter
@@ -17,6 +20,7 @@ from channels.security.websocket import AllowedHostsOriginValidator
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stockproject.settings')
+django.setup()
 
 application = ProtocolTypeRouter(
     {
