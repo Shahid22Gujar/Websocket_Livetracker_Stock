06783ba8ff6f09f89b46286bb3d6be37d8018b2f
Shahid22Gujar <shahidahimadgujar86@gmail.com>
2022-11-28 19:39:02 +0530

    vercel deployment ready

--- stockproject/wsgi.py
+++ stockproject/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stockproject.settings')
 
 application = get_wsgi_application()
+#app is for vercel only app is recognized by vercel
+app=application
